a3b6f8bab3b85ab2c5b88e25fc88d5c8f69a29bd
Kolexas <maksimilyan.klimentev39@gmail.ru>
2024-11-28 19:40:27 +0300

    Initial commit

---
+++ src/org/skypro/skyproshop/ProductBasket.java
@@ -0,0 +1,63 @@
+package org.skypro.skyproshop;
+
+import java.util.Arrays;
+
+public class ProductBasket {
+    Product[] basket = new Product[5];
+
+    public int findSpace() {
+        for (int i = 0; i < basket.length; i++) {
+            if (basket[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void addProduct(Product product) {
+        int i = findSpace();
+        if (i == -1) {
+            System.out.println("Невозможно добавить продукт");
+            return;
+        }
+        basket[i] = product;
+    }
+
+    public int totalPrice() {
+        int total = 0;
+        for (Product product : basket) {
+            if (product != null) {
+                total += product.getPrice();
+            }
+        }
+        return total;
+    }
+
+    public void listOfProducts() {
+        int nullNum = 0;
+        for (Product product : basket) {
+            if (product == null) {
+                nullNum++;
+            } else {
+                System.out.println(product);
+            }
+        }
+        if (nullNum == basket.length) {
+            System.out.println("в корзине пусто");
+        } else System.out.println("Итого: " + totalPrice());
+    }
+
+    public boolean isProductInBasket(String name) {
+        for (Product product : basket) {
+            if (product != null && name.equals(product.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clearBasket() {
+        Arrays.fill(basket, null);
+    }
+}
+
