3302513663d2c304597eb418e5bd9be6d2c32338
Kolexas <maksimilyan.klimentev39@gmail.ru>
2024-12-03 19:50:14 +0300

    Initial commit: Inheritance

--- src/org/skypro/skyproshop/App.java
+++ src/org/skypro/skyproshop/App.java
@@ -3,15 +3,15 @@ package org.skypro.skyproshop;
 public class App {
     public static void main(String[] args) {
         ProductBasket basket1 = new ProductBasket();
-        basket1.addProduct(new Product("Банан", 150));
-        basket1.addProduct(new Product("Клубника", 300));
-        basket1.addProduct(new Product("Хлеб", 80));
-        basket1.addProduct(new Product("Мандарины", 100));
-        basket1.addProduct(new Product("Йогурт", 50));
-        basket1.addProduct(new Product("Колбаса", 600));
+        basket1.addProduct(new SimpleProduct("Банан", 150));
+        basket1.addProduct(new SimpleProduct("Клубника", 300));
+        basket1.addProduct(new SimpleProduct("Хлеб", 80));
+        basket1.addProduct(new SimpleProduct("Мандарины", 100));
+        basket1.addProduct(new SimpleProduct("Йогурт", 50));
+        basket1.addProduct(new SimpleProduct("Колбаса", 600));
         ProductBasket basket2 = new ProductBasket();
-        basket2.addProduct(new Product("Колбаса", 600));
-        basket2.addProduct(new Product("Cыр", 300));
+        basket2.addProduct(new SimpleProduct("Колбаса", 600));
+        basket2.addProduct(new SimpleProduct("Cыр", 300));
         basket2.listOfProducts();
         System.out.println(basket2.totalPrice());
         System.out.println(basket2.isProductInBasket("Колбаса"));
@@ -20,5 +20,12 @@ public class App {
         basket2.listOfProducts();
         System.out.println(basket2.totalPrice());
         System.out.println(basket2.isProductInBasket("Йогурт"));
+        basket2.addProduct(new FixPriceProduct("Малина"));
+        basket2.addProduct(new DiscountedProduct("Колбаса", 10, 600));
+        basket2.addProduct(new FixPriceProduct("Малина"));
+        basket2.addProduct(new DiscountedProduct("Колбаса", 10, 600));
+        basket2.addProduct(new SimpleProduct("Мандарины", 100));
+        basket2.listOfProducts();
+
     }
 }
