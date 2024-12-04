612a3cf40d53a33da964625b68d92c38be14e799
Kolexas <maksimilyan.klimentev39@gmail.ru>
2024-12-04 19:30:49 +0300

    Move classes in separate files. Add access Modifiers to variables.

--- src/org/skypro/skyproshop/Product.java
+++ src/org/skypro/skyproshop/Product.java
@@ -17,7 +17,6 @@ public abstract class Product {
 
     public abstract int getPrice();
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -31,72 +30,3 @@ public abstract class Product {
         return Objects.hashCode(name);
     }
 }
-
-class SimpleProduct extends Product {
-    private int price;
-
-    public SimpleProduct(String name, int price) {
-        super(name);
-        this.price = price;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-
-    @Override
-    public String toString() {
-        return getName() + ": " + price;
-    }
-
-    public boolean isSpecial() {
-        return false;
-    }
-}
-
-class DiscountedProduct extends Product {
-    int basePrice;
-    float discount;
-
-    public DiscountedProduct(String name, int discount, int basePrice) {
-        super(name);
-        this.discount = discount;
-        this.basePrice = basePrice;
-    }
-
-
-    public int getPrice() {
-        return (int) (basePrice * (1 - discount / 100f));
-    }
-
-    public String toString() {
-        return getName() + " со скидкой: " + getPrice();
-    }
-
-    public boolean isSpecial() {
-        return true;
-    }
-}
-
-
-class FixPriceProduct extends Product {
-    static final int FIX_PRICE = 1551;
-
-    public FixPriceProduct(String name) {
-        super(name);
-    }
-
-    public int getPrice() {
-        return FIX_PRICE;
-    }
-
-    public String toString() {
-        return getName() + " : Фиксированная цена " + FIX_PRICE;
-    }
-
-    public boolean isSpecial() {
-        return true;
-    }
-}
-
